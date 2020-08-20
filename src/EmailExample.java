14f201cf812cbfa2f8a55b6470b3319797fa9155
ngocqui1996 <hivongmotloihua@yahoo.com>
2020-08-20 23:05:16 +0700

    sua lai

--- src/EmailExample.java
+++ src/EmailExample.java
@@ -5,7 +5,7 @@ public class EmailExample {
     private static Pattern pattern;
     private Matcher matcher;
 
-    private static final String EMAIL_REGEX =   "^(?=[a-z]*\\d).{6,15}@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    private static final String EMAIL_REGEX =   "^(?=[a-z]*\\d)[a-z0-9]{6,15}@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
 
     // So với "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$"    này thì không bắt buôc có ký tự số
     // Còn tham chiếu EMAIL_REGEX định dạng regex thì bắt buộc phải có ít nhất 1 ký tự số.
